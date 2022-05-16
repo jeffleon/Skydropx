d664af32d0479931838e19ef1f7bc116a44f16a9
jeffleon <jefferson.leon@usantotomas.edu.co>
2022-05-15 23:02:03 -0500

    Add repository labelRequest and Add DTO for shipping information

--- src/core/repositories/labelReques.repository.ts
+++ src/core/repositories/labelReques.repository.ts
@@ -1,3 +1,10 @@
+import Carrier from "../entities/Carrier";
+
+
 export default interface LabelRequestRepository {
-    
+    saveRequest(carrier: string): Promise<Carrier>;
+    getRequestById(requestId:string):Promise<Carrier>;
+    updateRequestStatus(requestId:string, status: string):Promise<Carrier>;
+    updateURLZip(requestId:string, url: string):Promise<Carrier>;
+    downloadZip(requestId:string):Promise<any>;
 }
---
+++ src/dto/shippingInformation.ts
@@ -0,0 +1,31 @@
+export interface ShippingInformationDTO {
+    carrier: string;
+    shipment: ShippingDetailsObject;
+}
+
+export interface ShippingDetailsObject {
+    address_from: AddressI;
+    address_to: AddressI;
+    parcels: ParcelsI[];
+}
+
+export interface AddressI {
+    name: string;
+    street1: string;
+    city: string;
+    province: string;
+    postal_code: string;
+    country_code: string;
+} 
+
+export interface ParcelsI {
+    length: number;
+    width: number;
+    height: number;
+    dimensions_unit: string;
+    weight: number;
+    weight_unit: string;
+}
+
+
+
