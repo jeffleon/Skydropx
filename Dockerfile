651a27266e4189e29cc85050f227fe06037e8206
jeffleon <jefferson.leon@usantotomas.edu.co>
2022-05-22 00:52:29 -0500

    update package.json dockerfile

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,4 @@ COPY . .
 
 EXPOSE 8080
 
-CMD [ "npm", "run", "watch"]
+CMD [ "npm", "run", "start"]
--- src/core/interactors/requestLabel/create/saveRequest.interactor.ts
+++ src/core/interactors/requestLabel/create/saveRequest.interactor.ts
@@ -5,21 +5,22 @@ import { fork } from "child_process";
 import path from "path";
 import { ShippingInformationType } from "../../../../types/shippingInformation";
 import JwtRepository from "../../../repositories/jwt.repository";
+import fs from "fs";
 
 /**
- * This interactor inyect the dependencies of a labelRequestRepository then call the method 
- * to save the request label and begin a subprocess 
+ * This interactor inyect the dependencies of a labelRequestRepository then call the method
+ * to save the request label and begin a subprocess
  * to create the zip file also update the state of requests
  * return the request created
  * @param labelRequestRepository
  * @param shippingInfo
- * @return 
+ * @return
  * @returns Promise<RequestLabel> | Error
  */
 const saveRequest = (labelRequestRepository:LabelRequestRepository, jwt:JwtRepository) => async (shippingInfo:ShippingInformationType[]) => {
     const id: string = uuid();
     let carrierName: string = "";
-    
+
     if (shippingInfo && Array.isArray(shippingInfo) && shippingInfo.length > 0 && shippingInfo[0].hasOwnProperty('carrier')){
         carrierName = shippingInfo[0].carrier;
     } else {
@@ -36,7 +37,8 @@ const saveRequest = (labelRequestRepository:LabelRequestRepository, jwt:JwtRepos
     };
     try {
         const resquestLabelPost = await labelRequestRepository.save(requestLabel);
-        const childProcess = fork(path.join(__dirname, '..', '..', 'zipPdf', 'create', 'zipPdfGenerator.interactor.ts'));
+        const files = fs.readdirSync(path.join(__dirname, '..', '..', 'zipPdf', 'create'));
+        const childProcess = fork(path.join(__dirname, '..', '..', 'zipPdf', 'create', files[0]));
         childProcess.send({shippingInfo, id});
         const response = await labelRequestRepository.updateStatus(id, "procesing");
         return  resquestLabelPost;
